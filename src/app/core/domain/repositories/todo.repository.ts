1d357ee3b8c6949d039642e5554af9f9437ec927
Boris Bonett <borisbonett@MacBook-Pro-de-Boris.local>
2026-04-21 11:43:46 -0500

    Repositorios de nuestra aplicación

---
+++ src/app/core/domain/repositories/todo.repository.ts
@@ -0,0 +1,12 @@
+import { Observable } from 'rxjs';
+import { Task, Category } from '../models/todo.model';
+
+export abstract class TaskRepository {
+  abstract getTasks(): Observable<Task[]>;
+  abstract saveTasks(tasks: Task[]): Observable<void>;
+}
+
+export abstract class CategoryRepository {
+  abstract getCategories(): Observable<Category[]>;
+  abstract saveCategories(categories: Category[]): Observable<void>;
+}
